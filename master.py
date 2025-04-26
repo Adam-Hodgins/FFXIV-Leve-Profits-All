3959f7b426570ae3f6b5efbd875ad66e5b6fd015
Equinar <hodginsbusiness@gmail.com>
2025-04-26 15:15:25 -0400

    Show Last Update TimeStamp on Site

--- master.py
+++ master.py
@@ -1,5 +1,7 @@
 import subprocess
 import sys
+import datetime
+from datetime import timezone
 
 SCRIPTS = [
     'multi_world_fetch.py',
@@ -16,11 +18,16 @@ def run_script(script_name):
         print(f"Error: {script_name} exited with code {e.returncode}")
         sys.exit(e.returncode)
 
+def update_timestamp():
+    with open ("LastUpdate.txt", "w") as text_file:
+        text_file.write(str(now))
+    
 
 def main():
     for script in SCRIPTS:
         run_script(script)
     print("\nAll tasks completed successfully.")
+    update_timestamp()
 
 
 if __name__ == '__main__':
